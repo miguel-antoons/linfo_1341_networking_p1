81bbd365b19ed1ffe4a53279d2814ff10c25d35f
MaximeDanlee <max@interplanet.be>
2023-04-08 17:30:43 +0200

    Add piechart

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pyshark
 import time
+import matplotlib.pyplot as plt
 
 def call_with_video():
-    capture = pyshark.FileCapture('captures/call_full.pcapng', display_filter="udp and ip.addr==20.202.145.223")
+    capture = pyshark.FileCapture('captures/call_full_supernode.pcapng', display_filter="udp and ip.addr==20.202.145.223")
 
     total_bytes = 0
     start_time = 13
@@ -24,8 +25,10 @@ def call_with_video():
     print("Nombre de paquets échangés pendant l'appel Skype avec vidéo: ", nb_packets, "paquets/min")
     print("")
 
+    capture.close()
+
 def call_without_video():
-    capture = pyshark.FileCapture('captures/voice_call_full.pcapng', display_filter="udp and ip.addr==109.136.186.101")
+    capture = pyshark.FileCapture('captures/voice_call_full_p2p.pcapng', display_filter="udp and ip.addr==109.136.186.101")
 
     total_bytes = 0
     start_time = 13
@@ -45,7 +48,68 @@ def call_without_video():
     print("Volume de données échangées pendant l'appel Skype sans vidéo : ", total_bytes, "bytes/min")
     print("Nombre de paquets échangés pendant l'appel Skype sans vidéo : ", nb_packets, "paquets/min")
 
+    capture.close()
+
+def get_destination_ip(capture_file, src_ip):
+    capture = pyshark.FileCapture(capture_file, display_filter="ip.src == " + src_ip)
+    dest_ips = {}
+
+    total_packets = 0
+    for packet in capture:
+        total_packets += 1
+        if 'IP' in packet:
+            dest_ip = packet['IP'].dst
+
+            if dest_ip in dest_ips:
+                dest_ips[dest_ip] += 1
+            else:
+                dest_ips[dest_ip] = 1
+
+    # Calculer le pourcentage pour chaque adresse IP de destination
+    dest_ip_percentages = {}
+    for dest_ip, frequency in dest_ips.items():
+        percentage = (frequency / total_packets) * 100
+        dest_ip_percentages[dest_ip] = percentage
+
+    # Trier les adresses IP de destination par ordre décroissant de fréquence
+    percentage_sorted = sorted(dest_ip_percentages.items(), key=lambda x: x[1], reverse=True)
+
+    capture.close()
+
+    return percentage_sorted
+    
+def create_pie_chart(capture_file, src_ip):
+    dest_ip_percentages = get_destination_ip(capture_file, src_ip)
+
+    threshold = 10
+
+    labels = []
+    sizes = []
+
+    for dest_ip, percentage in dest_ip_percentages:
+        if percentage >= threshold:
+            labels.append("Proximus")
+            sizes.append(percentage)
+
+    labels.append("Other")
+
+    print(sizes)
+
+    other_percentage = 100 - sum(sizes)
+
+    sizes.append(other_percentage)
+
+    # Create a pie chart
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 12})
+    ax.axis('equal')
+
+    # Show the chart
+    plt.show()
 
 if __name__ == "__main__":
-    call_with_video()
-    call_without_video()
+    # call_with_video()
+    # call_without_video()
+    #get_destination_ip()
+    #create_pie_chart('captures/call_full_supernode.pcapng', "172.16.177.129")
+    create_pie_chart('captures/video_call_2.pcapng', "172.16.177.128")
